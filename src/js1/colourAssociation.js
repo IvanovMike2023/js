87913115d08f329f5ce7d3ef8b77fdb4d9804e45
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-10-09 23:57:08 +0300

    colourAssociation

--- src/js1/colourAssociation.js
+++ src/js1/colourAssociation.js
@@ -2,16 +2,23 @@ function colourAssociation(array){
     // We <3 colours..
     //console.log(array.toString().split(','))
     let res= new Object()
-    let mas = array.toString().split(',')
-    //let key = mas[0]
-res[mas[0]]=mas[1]
-    console.log(mas)
-    mas.map((m,a,l)=>{
-        console.log(l.length)
-        //res[]
+    let res2= new Object()
+    let mas = array//.toString().split(',')
+
+//res[mas[0]]=mas[1]
+
+
+    //console.log(mas)
+    mas.map((m)=>{
+
+    res[m[0]] = m[1]
+        res2={...res}
     })
     console.log(res)
-
+    let newr = []
+    newr.push(res2)
+    // mas.push(res)
+    console.log(newr)
 }
 var array = [["white", "goodness"],["whinfgnfgte", "goodnessnnfn"]] //returns [{white: 'goodness'}, ...]
 colourAssociation(array)
