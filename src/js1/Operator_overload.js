9f4646f4160f23dd7971f560cd2c48e38e214dd3
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-10-24 19:30:25 +0300

    Operator overload

--- src/js1/Operator_overload.js
+++ src/js1/Operator_overload.js
@@ -2,6 +2,6 @@ var Foo = function(value) {
     this.val = value;
     return Object.assign(this.val)
 }
-var foo = new Foo(2);     // foo.val = 2;
+var foo = new Foo(2);
 var boo = new Foo(3);
 console.log(foo + boo) // => must return 5
