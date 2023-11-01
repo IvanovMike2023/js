c9c44ace020f42d335e833f71b79df26b6d499eb
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-11-01 11:33:01 +0300

    6 kyu Who likes it?!

--- src/kata_6_key/Find_the_odd_int.js
+++ src/kata_6_key/Find_the_odd_int.js
@@ -1,19 +1,19 @@
 function findOdd(A) {
-    let count=0
-let r= A.filter((a,pos)=>{
-        return A.indexOf(a)===pos
+    let count = 0
+    let r = A.filter((a, pos) => {
+        return A.indexOf(a) === pos
     })
-    for(let i=0;i<r.length;i++){
-        let pos =A.indexOf(r[i])
-        while (pos!=-1){
+    for (let i = 0; i < r.length; i++) {
+        let pos = A.indexOf(r[i])
+        while (pos != -1) {
             count++
-            pos=A.indexOf(r[i],pos+1)
+            pos = A.indexOf(r[i], pos + 1)
         }
-        if(count%2!=0){
-          return r[i]
+        if (count % 2 != 0) {
+            return r[i]
         }
-        count=0
+        count = 0
     }
 };
 //findOdd([0,1,0,1,0])
-findOdd([1,2,2,2,4,4,1])
+findOdd([1, 2, 2, 2, 4, 4, 1])
--- src/kata_6_key/Who_likes_it.js
+++ src/kata_6_key/Who_likes_it.js
@@ -1,3 +1,24 @@
 function likes(names) {
+    let text = 'likes this'
+    let text2 = 'like this'
+    console.log(names=='')
+    if(names==''){
+        console.log('no one likes this')
+    }
+    else if(names.length===1) {
+        console.log(names.toString() + ' ' + text)
+    }else if(names.length===2){
+        console.log(names[0].toString() + ' and ' +names[1].toString() +' ' + text)
+    }else if(names.length===3){
+        console.log(names[0].toString() +','+ names[1].toString() +' and ' +names[2].toString() +' ' + text)
+    }else if(names.length>3){
+        console.log(names.length-2)
+        console.log(names[0].toString() +','+ names[1].toString() +` and ${names.length-2} others ` +' ' + text)
+    }
     // TODO
 }
+likes([])   //                             -->  "no one likes this"
+     //likes(["Peter"])        //                 -->  "Peter likes this"
+//likes(["Jacob", "Alex"]) //                -->  "Jacob and Alex like this"
+//likes(["Max", "John", "Mark"])//           -->  "Max, John and Mark like this"
+//likes(["Alex", "Jacob", "Mark", "Max", "Max", "Mark", "Max", "Max"]) //  -->  "Alex, Jacob and 2 others like this"
