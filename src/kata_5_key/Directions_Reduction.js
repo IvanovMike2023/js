6d4da62053f159df8929678376750a512f8544f8
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-11-09 00:22:00 +0300

    dirReduc

--- src/kata_5_key/Directions_Reduction.js
+++ src/kata_5_key/Directions_Reduction.js
@@ -1,27 +1,54 @@
-function dirReduc(arr) {
-    // ...
-    let mas = arr
-let perebor = (m)=>{
-    for (let i=0;i<m.length;i++){
-        if(m[i]==="NORTH" && m[i+1]==="SOUTH") {
-            let r
-            [,,...r]=m
-            console.log(r)
-           perebor(r)
-        }
-        if(m[i]==="EAST" && m[i+1]==="WEST") {
-            let k
-            ["EAST","WEST",...k]=m
-            console.log(k)
-           perebor(k)
-        }
-        else console.log('xsaxasxsa')
-    }
-}
-    perebor(mas)
-}
+function dirReduc  (m)  {
+    let mas2=[]
 
+    let someF=(a,b,c)=>{
+        // if(c[b+1])
+        return c[b+1] + ' '+c[b] ==="EAST WEST" || c[b+1] + ' '+c[b] ==="WEST EAST" || c[b+1] + ' '+c[b] ==="NORTH SOUTH" || c[b+1] + ' '+c[b] ==="SOUTH NORTH"
+    }
+    debugger
+    let recurs = (m)=>{
+        for (let i = 0; i < m.length; i++) {
+
+            if (m[i] === "NORTH" && m[i + 1] === "SOUTH") {
+                m.splice(i,1)
+                m.splice(i,1)
+
+                recurs(m)
+            }
+            if (m[i] === "EAST" && m[i + 1] === "WEST") {
+                m.splice(i,1)
+                m.splice(i,1)
+                recurs(m)
+            }
+
+            if (m[i] === "WEST" && m[i + 1] === "EAST") {
+                m.splice(i,1)
+                m.splice(i,1)
+                recurs(m)
+            }
+            if (m[i] === "SOUTH" && m[i + 1] === "NORTH") {
+                m.splice(i,1)
+                m.splice(i,1)
+                mas2=m
+                recurs(m)
+
+            }
+            if(m.some(someF)){
+
+            }
+            else
+                return mas2=m
+        }
+    }
+    recurs(m)
+    console.log(mas2)
+    return mas2
+    // return mas2
+}
 //dirReduc( ["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST","WEST"])  ["NORTH","SOUTH","SOUTH","EAST","WEST","NORTH","WEST"]
+//dirReduc(["NORTH", "SOUTH", "WEST"])
 dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])
+//dirReduc( ["EAST","EAST","WEST","NORTH","WEST","EAST","EAST","SOUTH","NORTH","WEST"]
+
 // ["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"],
 //     []
