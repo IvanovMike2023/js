b56d7aca103f64df963d8557c1461867fa5c0ddd
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-11-14 16:57:29 +0300

    Calculating with Functionsscac

--- src/kata_5_key/Product_of_consecutive_Fib_numbers.js
+++ src/kata_5_key/Product_of_consecutive_Fib_numbers.js
@@ -1,24 +1,34 @@
-
-let productFib=(n)=>{
-let result =[]
-    function fib(x) {
-        let cur = 1, prev = 1;
-        for (let i = 3; i <= x; i++) {
-            [cur, prev] = [cur + prev, cur];
-        }
-        return cur ;
+//
+// let productFib=(n)=>{
+// let result =[]
+//     function fib(x) {
+//         let cur = 1, prev = 1;
+//         for (let i = 3; i <= x; i++) {
+//             [cur, prev] = [cur + prev, cur];
+//         }
+//         return cur ;
+//     }
+//   for (let i=1;i<=n;i++){
+//       if(fib(i)*fib(i+1)===n){
+//           result.push(fib(i),fib(i+1),true)
+//           console.log(result)
+//       }
+//       if(fib(i)*fib(i+1)<n && fib(i+1)*fib(i+2)>n){
+//           result.push(fib(i+1),fib(i+2),false)
+//           console.log(result)
+//       }
+//
+//   }
+// }
+function productFib(prod){
+    debugger
+    let n = 0;
+    let nPlus = 1;
+    while(n*nPlus < prod) {
+        nPlus = n + nPlus;
+        n = nPlus - n;
     }
-  for (let i=1;i<=n;i++){
-      if(fib(i)*fib(i+1)===n){
-          result.push(fib(i),fib(i+1),true)
-          console.log(result)
-      }
-      if(fib(i)*fib(i+1)<n && fib(i+1)*fib(i+2)>n){
-          result.push(fib(i+1),fib(i+2),false)
-          console.log(result)
-      }
-
-  }
+    return console.log([n, nPlus, n*nPlus===prod]);
 }
 //productFib(4895)
 productFib(5895)
