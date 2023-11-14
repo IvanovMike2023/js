8f94abf34ecc1aa3f0b276ccc944a2d2dc0f7904
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-11-14 16:50:34 +0300

    Calculating with Functions

---
+++ src/kata_5_key/Product_of_consecutive_Fib_numbers.js
@@ -0,0 +1,29 @@
+
+let productFib=(n)=>{
+let result =[]
+    function fib(x) {
+        let cur = 1, prev = 1;
+        for (let i = 3; i <= x; i++) {
+            [cur, prev] = [cur + prev, cur];
+        }
+        return cur ;
+    }
+  for (let i=1;i<=n;i++){
+      if(fib(i)*fib(i+1)===n){
+          result.push(fib(i),fib(i+1),true)
+          console.log(result)
+      }
+      if(fib(i)*fib(i+1)<n && fib(i+1)*fib(i+2)>n){
+          result.push(fib(i+1),fib(i+2),false)
+          console.log(result)
+      }
+
+  }
+}
+//productFib(4895)
+productFib(5895)
+//productFib(714)
+// //F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+// productFib(714) # should return (21, 34, true),
+// # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
