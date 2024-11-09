344ebc692ddabb0bc2b53e04537bcda9c5cbe63e
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2024-11-09 23:39:40 +0300

    ssss

--- src/kata_6_key/Two_Sum.js
+++ src/kata_6_key/Two_Sum.js
@@ -1,18 +1,15 @@
 function twoSum(numbers, target) {
-    console.log(numbers)
-    const initialValue = 0//numbers[0];
-    const sumWithInitial = numbers.reduce(
-        (accumulator, currentValue) => {
-            debugger
-            console.log(currentValue+accumulator)
-            return     accumulator + currentValue
-
-        },initialValue
-    );
-    console.log(sumWithInitial)
-    return [0, 1];
+    let mas=[]
+    for(let i=0;i<numbers.length;i++){
+        for(let j=i+1;j<numbers.length;j++){
+           if(numbers[i]+numbers[j]==target){
+             mas.push(i,j)
+           }
+        }
+    }
+    return mas;
 }
-twoSum([1, 2, 3], 4)
+twoSum([2, 3, 1], 3)
     // doTest([1, 2, 3], 4);
     // doTest([1234, 5678, 9012], 14690);
     // doTest([2, 2, 3], 4);
