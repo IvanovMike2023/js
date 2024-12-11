654d3607f1d9365b477d2ba8ca3d6d3027ab8254
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2024-12-11 09:41:01 +0300

    toCamelCase

---
+++ src/kata_6_key/toCamelCase.js
@@ -0,0 +1,6 @@
+function toCamelCase(str){
+     str = str.replace(/[^a-zа-яё0-9\s]/gi, ' ');
+    const capitalize = s => s.replace(/[^A-z]\b\w/g, c => c.toUpperCase());
+   return str=capitalize(str).replaceAll(' ','')
+}
+toCamelCase("Rhe_stealth_warrior")
