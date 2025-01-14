10fc2151ae62c77c39981a1bbb63bd932b65c715
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2025-01-14 10:51:27 +0300

    Class!!sdcascas

--- src/kata_6_key/Duplicate_Encoder.js
+++ src/kata_6_key/Duplicate_Encoder.js
@@ -1,20 +1,31 @@
 function duplicateEncode(word) {
-    const arrayword = word.split('')
-    console.log(arrayword.indexOf('d'))
-    // ...
-    for (let i = 0; i < arrayword.length; i++) {
-        //console.log(arrayword.indexOf(arrayword[i]))
-        //console.log(arrayword.includes(arrayword[i]))
-        if (arrayword.indexOf(arrayword[i]) === i) {
-            arrayword.splice(i,1, ')')
-        }
-        else arrayword.splice(i, 1,'1')
+    const arrayword = word.toLowerCase().split('')
+    const countItems = {};
+    for (const item of arrayword) {
+        // если элемент уже был, то прибавляем 1, если нет - устанавливаем 1
+        countItems[item] = countItems[item] ? countItems[item] + 1 : 1;
     }
+    //console.log(countItems['i'] ? countItems['i'] + 1: console.log('2'))
     console.log(arrayword)
-    return '';
+    // console.log(countItems['d']=countItems['d'] ? countItems['d'] + 1 : 1)
+    const result = Object.keys(countItems).filter((item) => countItems[item] > 1);
+    console.dir(result);
+    arrayword.map((el, j) => {
+        for (let i = 0; i < result.length; i++) {
+            if (el === result[i]) {
+                //console.log(el)
+                arrayword.splice(j, 1, ')')
+            }//else console.log(el,j)
+            else arrayword.splice(j, 1, '(')
+        }
+    })
+    // arrayword.splice(1,1,')')
+    console.log(arrayword)
+    return arrayword;
 }
 
-duplicateEncode("ddin")//,"(((")
+duplicateEncode("succdins")//,"(((")
+//duplicateEncode([1,2,1,1])//,"(((")
 // duplicateEncode("din","(((")
-// duplicateEncode("Success",")())())","should ignore case")
+//duplicateEncode("Success")//,")())())","should ignore case")
 // duplicateEncode("(( @","))((")
