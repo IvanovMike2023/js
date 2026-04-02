a602bacba49e6ccd47850f4c9ad67faa10f28dcf
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-04-02 19:06:52 +0300

    backwardsPrime

---
+++ src/kata_6_key/backwardsPrime.js
@@ -0,0 +1,33 @@
+function backwardsPrime(start, stop){
+    // your code here
+    const mas=[]
+    for(let i=start;i<=stop;i++){
+        mas.push(i)
+    }
+    function isPrime(n) {
+        if (n <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    const res2=mas.filter((f)=>isPrime(f)===true).filter((f)=>f!=2 & f!=3 & f!=5& f!=7& f!=9)
+    function reverseNumber(n) {
+        const reversed = parseFloat(
+            n.toString().split('').reverse().join('')
+        );
+
+        if (reversed === n) {
+            return false; // число равно самому себе
+        }
+        return reversed;
+    }
+    const res3=res2.map((m)=>reverseNumber(m)).filter((f)=>isPrime(f)===true).map((m)=>reverseNumber(m))
+    console.log(res3)
+
+}
+//(start = 2, end = 100) => [13, 17, 31, 37, 71, 73, 79, 97]
+//backwardsPrime(2,100)
+backwardsPrime(9900,10000)
