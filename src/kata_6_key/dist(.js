d9aadc537d9742fe538c95b4cdb345ecd05b9a25
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2026-04-03 22:02:11 +0300

    dist

---
+++ src/kata_6_key/dist(.js
@@ -0,0 +1,19 @@
+function dist(v, mu) {
+    const g = 9.81;
+    const t = 1;
+
+    const vms = v / 3.6; // km/h -> m/s
+
+    const reaction = vms * t;
+    const braking = (vms * vms) / (2 * mu * g);
+
+    return reaction + braking;
+}
+
+function speed(d, mu) {
+    const g = 9.81;
+
+    const vms = (-2 * mu * g + Math.sqrt((2 * mu * g) ** 2 + 8 * mu * g * d)) / 2;
+
+    return vms * 3.6; // m/s -> km/h
+}
